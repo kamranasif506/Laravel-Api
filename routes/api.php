a3edf69cb3d86707c5cfcbad1ee2d541457aa671
Kamran Asif <98281453+kamranasif506@users.noreply.github.com>
2025-08-15 22:49:44 +0500

    Setting up rest api routes

---
+++ routes/api.php
@@ -0,0 +1,16 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\PostController;
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+// Protected Routes
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/profile', [AuthController::class, 'profile']);
+    Route::post('/logout', [AuthController::class, 'logout']);
+
+    // CRUD Example
+    Route::apiResource('posts', PostController::class);
+});
